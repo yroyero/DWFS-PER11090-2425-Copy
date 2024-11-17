c1b050cd18fecfcdb006ff1b5c0923718541f8e1
Diego Gabriel <diego.guachamin990@gmail.com>
2024-11-17 13:46:43 -0500

    Tema 2 (#137)
    * Envio tarea 1
    
    * Mensaje que describa los cambios realizados
    
    * ejercicio 2
    
    * ejercicio 2
    
    * ejercicio 2
    
    ---------
    
    Co-authored-by: DiegoGuachamin <diego.guachamin@eommt.ec>
    Co-authored-by: UNIR - Software Engineering <115072043+UnirCs@users.noreply.github.com>

---
+++ Tema_2/Diego_Gabriel_Guachamin_Andrango/Cinema/cinema.js
@@ -0,0 +1,75 @@
+const rows = 5 // Número de filas
+const seat = 13 // Número de asientos por fila
+
+// Función para inicializar la matriz de SEAT
+function setup () {
+  let idContador = 1 // Iniciar el contador de IDs en 1
+  let butacas = []
+  for (let i = 0; i < rows; i++) {
+    // Nueva fila
+    let fila = []
+    for (let j = 0; j < seat; j++) {
+      // Nuevo asiento
+      fila.push({
+        id: idContador++,
+        estado: false, // Estado inicial libre
+      })
+    }
+    butacas.push(fila)
+//preseleccionar asiento
+//     function actualizarEstado(butacas, ids, nuevoEstado) {
+//       ids.forEach(id => {
+//         for (let fila of butacas) {
+//           for (let asiento of fila) {
+//             if (asiento.id === id) {
+//               asiento.estado = nuevoEstado;
+//             }
+//           }
+//         }
+//       });
+//     }
+//     actualizarEstado(butacas, [4, 7,8,9,10], true);
+  }
+  console.log(butacas)
+
+  return butacas
+
+}
+// Inicializar la matriz
+let butacas = setup()
+
+// Función para sugerir asientos
+function suggest (numAsientos) {
+  let reservados = new Set([])
+  let filaCorrecta = -1
+
+  if (numAsientos > seat) return reservados
+
+  for (let i = rows - 1; i >= 0 && filaCorrecta < 0; i--) {
+    const fila = butacas[i]
+    const asientosLibres = fila.filter((asiento) => !asiento.estado).length
+    if (asientosLibres >= numAsientos) {
+      filaCorrecta = i
+      // Marcar Reservados
+      let marcados = numAsientos
+      for (let j = 0; j < seat && marcados > 0; j++) {
+        const reservado = butacas[i][j]
+        if (reservado.estado === false) {
+          reservado.estado = true
+          reservados.add(reservado.id)
+          marcados -= 1
+        }
+      }
+    }
+  }
+
+  // Devolver resultado:
+  return reservados
+}
+
+// Ejemplos de uso
+console.log(suggest(1))
+console.log(suggest(1))
+console.log(suggest(7))
+console.log(suggest(4))
+console.log(suggest(14))
