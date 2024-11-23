91bf3fa3c2561cafd51b7347deb83c26f05a033e
César Ramírez Vega <cesar.ramirez197@comunidadunir.net>
2024-11-23 09:53:19 -0600

    ejercicios tema 2 (#205)
    * add: ejercicios tema 1 (cv y cinema)
    
    * fix: estilos en linea eliminados
    
    * add: ejercicios tema 2
    
    ---------
    
    Co-authored-by: UNIR - Software Engineering <115072043+UnirCs@users.noreply.github.com>

---
+++ Tema_2/Cesar_Ramirez_Vega/cinema/cinema.js
@@ -0,0 +1,81 @@
+// Definir el tamaño de la matriz de butacas
+const N = 10; // Número de filas y columnas
+
+// Función para inicializar la matriz de butacas
+function setup() {
+  let idContador = 1; // Iniciar el contador de IDs en 1 (los humanos no empezamos a contar desde 0)
+  let butacas = [];
+
+  for (let i = 0; i < N; i++) {
+    // Nueva fila
+    let fila = [];
+    for (let j = 0; j < N; j++) {
+      // Nuevo asiento
+      fila.push({
+        id: idContador++,
+        estado: false // Estado inicial libre
+      });
+    }
+    butacas.push(fila);
+  }
+  return butacas;
+}
+
+// Inicializar la matriz
+let butacas = setup();
+
+
+// --------------------------------
+
+function suggest(numRequestedSeats) {
+  let suggestedSeats = new Set();
+
+  if (numRequestedSeats <= 0 || numRequestedSeats > N) {
+    return suggestedSeats;
+  }
+
+  for (let row = N - 1; row >= 0 && suggestedSeats.size < numRequestedSeats; row--) {
+    suggestedSeats.clear();
+    for (let seat = 0; seat < N && suggestedSeats.size < numRequestedSeats && N - seat >= numRequestedSeats - suggestedSeats.size; seat++) {
+      const { id, estado } = butacas[row][seat];
+
+      if (!estado) {
+        suggestedSeats.add(id);
+      } else {
+        suggestedSeats.clear();
+      }
+    }
+  }
+
+  return suggestedSeats;
+}
+
+// --------------------------------
+
+function printSeatsAsGrid(seats) {
+  console.log('');
+  seats.forEach(row => {
+    let rowStr = '';
+    row.forEach(seat => {
+      rowStr += ' ' + (seat.estado ? 'xx' : seat.id.toString().padStart(2, '0')) + ' ';
+    });
+    console.log(rowStr);
+  });
+  console.log('');
+}
+
+butacas[9][3].estado = true;
+butacas[9][4].estado = true;
+butacas[9][7].estado = true;
+butacas[9][8].estado = true;
+
+butacas[8][4].estado = true;
+butacas[8][5].estado = true;
+
+butacas[7][4].estado = true;
+butacas[7][5].estado = true;
+
+// --------------------------------
+
+printSeatsAsGrid(butacas);
+console.log("Suggested seats: ", suggest(3));
