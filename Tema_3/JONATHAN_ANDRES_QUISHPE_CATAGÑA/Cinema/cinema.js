06f8e6f0594a5c84e451f78a904ac8177bba76db
Jonathan Andrés <jonathanandres.quishpe263@comunidadunir.net>
2024-11-27 17:05:59 -0500

    Tema 3 (#243)
    * Tarea tema 1
    
    * Tema 2
    
    * Resolucion tarea 2
    
    * Tema 3
    
    ---------
    
    Co-authored-by: Fábrica Software 2 <sysdesarrolladorfsw2@usfq.edu.ec>
    Co-authored-by: UNIR - Software Engineering <115072043+UnirCs@users.noreply.github.com>

---
+++ Tema_3/JONATHAN_ANDRES_QUISHPE_CATAGÑA/Cinema/cinema.js
@@ -0,0 +1,49 @@
+// Definir el tamaño de la matriz de butacas
+const N = 10; // Número de filas y columnas
+
+// Función para inicializar la matriz de butacas
+function setup() {
+    console.log('Butacas iniciadas');
+    let idContador = 1; // Iniciar el contador de IDs en 1 (los humanos no empezamos a contar desde 0)
+    let butacas = [];
+
+    for (let i = 0; i < N; i++) {
+        // Nueva fila
+        let fila = [];
+        for (let j = 0; j < N; j++) {
+            // Nuevo asiento
+            fila.push({
+                id: idContador++,
+                estado: false // Estado inicial libre
+            });
+        }
+        butacas.push(fila);
+    }
+    return butacas;
+}
+
+function suggest(numSeats) {
+    const numColumns = butacas[0].length;
+
+    if (numSeats > numColumns) {
+        return new Set();
+    }
+
+    let availableSeats = [];
+    let reserved = false;
+    for (let row = butacas.length - 1; row >= 0; row--) {
+        for (let col = 0; col < numColumns; col++) {
+            if (!butacas[row][col].estado) {
+                if (!reserved) availableSeats.push(butacas[row][col].id);
+                if (availableSeats.length === numSeats) reserved = true;
+            } else {
+                availableSeats = [];
+            }
+        }
+    }
+
+    console.log(availableSeats);
+    return new Set(availableSeats);
+}
+
+const butacas = setup();
