d9e6dba860e7434a9cb4a532c83ae0ddcaedf7ee
Daniel Morón <94260698+danielmr6@users.noreply.github.com>
2024-12-08 21:11:15 +0100

    Ejercicio React (tema 5) (#312)
    * Tema 1 (CV y Cinema)
    
    * Ejercicios del Tema 2 (Cinema y Filtros)
    
    * Ejercicios tema 3
    
    * Ejercicios Tema 4
    
    * Ficheros fuera del PR
    
    * Ejercicios tema 5 (Frontend con React)
    
    * Enlace del deploy Cinema
    
    ---------
    
    Co-authored-by: UNIR - Software Engineering <115072043+UnirCs@users.noreply.github.com>

---
+++ Tema_5/Daniel_Moron_Roces/cinema/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+    return (
+        <footer className="footer-header">
+            <p className="footer-header-text">© 2024 UNIR Cinema</p>
+        </footer>
+    );
+}
---
+++ Tema_5/Daniel_Moron_Roces/cinema/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <footer className="footer-header">
+            <p className="footer-header-text">Bienvenios a UNIR Cinema</p>
+        </footer>
+    );
+}
