b60d4db7bf6836aa1c63865957d7a9a1b265d470
Yovani Royero Mena <yroyero@gmail.com>
2024-12-17 21:28:34 -0300

    Ejercicio Tema-5 (#325)
    * Ejercicio 1 completo
    
    * Ejercicio 2
    
    * Actividades Tema 2
    
    * Ejercicios Tema 3
    
    * Ejercicio Tema 4
    
    * navegacion
    
    * Actividad Tema-5-Yovani Royero
    
    ---------
    
    Co-authored-by: UNIR - Software Engineering <115072043+UnirCs@users.noreply.github.com>

---
+++ Tema_5/Yovani_Royero_Mena/cinema/src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.css';
+
+export const Header = () => {
+    return (
+        <div className="header">
+            <div className="banner">
+                <h1>Bienvenido a nuestro cine</h1>
+                <p>Disfrute de las mejores peliculas !</p>
+            </div>            
+        </div>
+    );
+};
